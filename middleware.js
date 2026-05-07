5965e07d6c72365c8e49b5ebfa9dd112b26401fc
ankurchangani <changaniankur100@gmail.com>
2026-05-07 16:26:37 +0530

    fix arcjet 403 issue

--- middleware.js
+++ middleware.js
@@ -11,11 +11,19 @@ const isProtectedRoute = createRouteMatcher([
 
 const aj = arcjet({
   key: process.env.ARCJET_KEY,
+  // characteristics: ["userId"], // Track based on Clerk userId
   rules: [
-    shield({ mode: "DRY_RUN" }), // ✅ FIX
-    detectBot({
+    // Shield protection for content and security
+    shield({
       mode: "LIVE",
-      allow: ["CATEGORY:SEARCH_ENGINE", "GO_HTTP"],
+    }),
+    detectBot({
+      mode: "LIVE", // will block requests. Use "DRY_RUN" to log only
+      allow: [
+        "CATEGORY:SEARCH_ENGINE", // Google, Bing, etc
+        "GO_HTTP", // For Inngest
+        // See the full list at https://arcjet.com/bot-list
+      ],
     }),
   ],
 });
